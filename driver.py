c2b21f3421c9a315712aa7fbc23ae97fa882a919
NathanUTD2140 <167475701+NathanUTD2140@users.noreply.github.com>
2025-10-19 20:48:09 -0500

    Update driver.py
    Putting in the base case for quit and password. I will implement encrypt and decrypt next. Includes a function called log line, as each case will commit a different message and give it to the log file.

--- driver.py
+++ driver.py
@@ -30,5 +30,52 @@ def main():
 
     # List to store session history of inputs/results
     history = [] # history is a blank array full of user inputs for encryption and decryption
+    
+    def log(action, msg):
+        """Helper to send messages to the logger process."""
+        logger.stdin.write(f"{action} {msg}\n") # write what happened in log file with the action and messaged associated.
+        logger.stdin.flush()
 
+    # Log for specifically start, unique message
+    log("START", "Driver started.")
+
+    try:
+        while True:
+            # Ask the user for their input
+            cmd = input("\nCommand (password/encrypt/decrypt/history/quit): ").strip().lower() # will continue to show up
+
+            # QUIT
+            if cmd == "quit":
+                log("QUIT", "Driver exiting.")
+
+                # Send quit commands to other process.
+                encryption.stdin.write("QUIT\n")  # tell them what happened
+                encryption.stdin.flush()
+                logger.stdin.write("QUIT\n")
+                logger.stdin.flush()
+
+                # Waits on both processes to close
+                encryption.wait()
+                logger.wait()
+                break # stop the loop
+
+            # PASSWORD
+            elif cmd == "password":
+                password = input("Enter passkey (letters only): ").strip().upper() # gets rid of white space and converts to upper case (not case sensitive)
+                if not password.isalpha():
+                    print("Error: only letters allowed.") # only alphabet characters
+                    log("ERROR", "Invalid passkey.") # write to log that it was an error with message
+                    continue
+
+                # give to encryption for it to know what to do
+                encryption.stdin.write(f"PASS {password}\n")
+                encryption.stdin.flush()
+
+                # give result from encryption
+                resp = encryption.stdout.readline().strip()
+                print(resp)
+
+                # Makes a log for this
+                log("COMMAND", "password")
+                log("RESULT", resp)
 
